87fdb7c06d16ffa039d9e78133df7daa246ffc14
jiiiieunnnnnnnnn <jer011025@naver.com>
2023-07-24 21:40:15 +0900

    hi

---
+++ 17069.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+m = [list(map(int,input().split())) for _ in range(n)]
+a = [[[0][0][0] for _ in range(n)] for _ in range(n)]
+a[0][1][0] = 1
+
+for i in range(1,n):
+    if m[0][i] == 0 :
+        a[0][i][0] = a[0][i-1][0]
+
+for i in range(1,n):
+    for j in range(1,n):
+        if m[i][j] == 0 and m[i][j-1] ==0 and m[i-1][j] ==0:
+            a[i][j][2] = a[i-1][j-1][0]+a[i-1][j-1][1]+a[i-1][j-1][2]
+        if m[i][j] == 0:
+            a[i][j][1] = a[i-1][j][1] + a[i-1][j][2]
+            a[i][j][0] = a[i][j-1][0] + a[i][j-1][2]
+print(sum(a[-1][-1]))
