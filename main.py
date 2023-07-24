1fbf75796db5084299a090aa58a3073592c8615a
jiiiieunnnnnnnnn <jer011025@naver.com>
2023-07-24 19:25:23 +0900

    hi

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-n = int(sys.stdin.readline())
-m = list(map(int, input().split()))
-a = [0]*(n+1)
-
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        a[i]=max(a[i], a[i-j]+m[j-1])
-print(a[n])
